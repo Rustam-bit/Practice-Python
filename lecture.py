01340fafc4b6f9dbaa9a3caaf0026253d389ccce
Rustam-bit <mirzaev.ra@phystech.edu>
2024-08-21 15:43:59 +0300

    Вторая задача

---
+++ lecture.py
@@ -0,0 +1,13 @@
+print(1 | 2)
+a = (
+    "python"
+    " is "
+    ' realy '
+    "exciting"
+)
+print(a)
+print("hello %s %d" % ("hi",123))
+print("hello {} {} ".format("hey",2222))
+var  = "World"
+var2 = 123
+print(f"Hello, {var} {var2}")
